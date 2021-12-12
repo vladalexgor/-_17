ffb253f7aea642c6dc70ab2cc3f36e372e419d2d
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-12-13 00:43:35 +0300

    Добавьте файлы проекта.

---
+++ Задание_17/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_17
+{
+    class Program
+    {
+        /*Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+        Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета.
+        Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. Создать  несколько экземпляров класса, параметризированного различными типами.
+        Заполнить его поля и вывести информацию об экземпляре класса на печать.*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ФИО:");
+            string fullName1 = Console.ReadLine();
+            Console.WriteLine("Введите номер счета:");
+            int accountNumber1 = Convert.ToInt32(Console.ReadLine());
+            Random random1 = new Random ();
+            int accountBalance1 = random1.Next(0,1000000);
+            BankAccount<int> bankAccount1 = new BankAccount<int>(accountNumber1, accountBalance1, fullName1);
+            bankAccount1.Print();
+            Console.WriteLine("Введите ФИО:");
+            string fullName2 = Console.ReadLine();
+            Console.WriteLine("Введите номер счета:");
+            string accountNumber2 = Console.ReadLine();
+            Random random2 = new Random();
+            int accountBalance2 = random2.Next(0, 1000000);
+            BankAccount<int> bankAccount2 = new BankAccount<int>(accountNumber1, accountBalance1, fullName1);
+            bankAccount2.Print();
+            Console.ReadKey();
+
+            /*print<int>(12);
+            print<string>("Hello!");
+            Console.ReadKey();
+
+            *//*Apartment<int,int> apartment1 = new Apartment<int,int>();
+            apartment1.Num = 1;
+            int next = apartment1.Num;
+
+            Apartment<string,int> apartment2 = new Apartment<string,int>();
+            apartment2.Num = "01";*//*
+        }
+        static void print<T>(T value)
+        {
+            Console.WriteLine(value);*/
+        }
+    }
+    class BankAccount<T>
+    {
+        private T AccountNumber { get; set; }
+        private int AccountBalance { get; set; }
+        private string FullName { get; set; }
+        public BankAccount(T accountNumber, int accauntBalance, string fullName)
+        {
+            AccountNumber = accountNumber;
+            AccountBalance = accauntBalance;
+            FullName = fullName;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Информация по счету:\nВладелец: {0}\nНомер счета: {1}\nНа вашем счету: {2} $", FullName, AccountNumber, AccountBalance);
+        }
+    }
+
+    /*class Apartment<T,U>
+    {
+        public U Level { get; set; }
+        public T Num { get; set; }
+    }*/
+}
